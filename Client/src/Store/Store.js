43501cd221bed6ab1cde5e22fb08948edc702cf7
Tejas Dhodi <tejasdhodi77@gmail.com>
2024-03-31 22:39:59 +0530

    Update Store

--- Client/src/Store/Store.js
+++ Client/src/Store/Store.js
@@ -4,13 +4,15 @@ import DishesSlice from "../Features/DishesSlice";
 import AuthSlice from "../Features/AuthSlice";
 import WishListSlice from "../Features/WishListSlice";
 import FilterSlice from "../Features/FilterSlice";
+import LoadingSlice from "../Features/LoadingSlice";
 const Store = configureStore({
     reducer: {
         cart: CartSlice,
         Dish: DishesSlice,
         authentication: AuthSlice,
         wishList: WishListSlice,
-        filters: FilterSlice
+        filters: FilterSlice,
+        loading: LoadingSlice
     }
 });
 
